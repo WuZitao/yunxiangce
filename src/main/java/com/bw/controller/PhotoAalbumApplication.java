a65b5e58a09da356f22c5c9e455252233ee10571
Hello <Hello@WuZitao>
2018-08-18 08:33:44 +0800

    第一次提交

---
+++ src/main/java/com/bw/controller/PhotoAalbumApplication.java
@@ -0,0 +1,12 @@
+package com.bw.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAalbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAalbumApplication.class, args);
+	}
+}
